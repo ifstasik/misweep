96b7f8bbad87ac55e3f14d3a6b1804d6bde910c3
slimey-code <slimey.in.mozilla@gmail.com>
2026-03-20 19:06:29 +0200

    field upgrade added tileOffset to change position of whole field made a func ConvertToTile(mousePosition). started updating SetBombs().

--- Minefield.cs
+++ Minefield.cs
@@ -22,7 +22,13 @@ public partial class Minefield : TileMapLayer
 		{ "seven",  new Vector2I(1, 7) },
 		{ "eight",  new Vector2I(1, 8) }
 	};
+	private Dictionary<Vector2I, string> Field = new Dictionary<Vector2I, string>();
 	
+	int mapWidth = 10;
+	int mapHeight = 20;
+	int bombNum = 10;
+	int offsetX = -5;
+	int offsetY = -10;
 	
 	private const int TargetAlternativeTile  = 0;
 	
@@ -30,35 +36,61 @@ public partial class Minefield : TileMapLayer
 	public override void _Ready()
 	{
 		rng.Randomize();
-		int mapWidth = 10;
-		int mapHeight = 20;
-		int bombNum = 10;
 		
-		FillMap(mapWidth, mapHeight);
-		SetBombs(mapWidth, mapHeight, bombNum);
+		
+		FillMap(mapWidth, mapHeight, offsetX, offsetY);
+		SetBombs(mapWidth, mapHeight, bombNum, offsetX, offsetY);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	private void FillMap(int width, int height)
+	private void FillMap(int width, int height, int offsetX, int offsetY)
 	{
 		for (int x = 0; x < width; x++)
 		{
 			for (int y = 0; y < height; y++)
 			{
-				SetCell(new Vector2I(x, y), TargetSourceId, Cells["closed"]);
+				SetCell(new Vector2I(x + offsetX, y + offsetY), TargetSourceId, Cells["closed"]);
 			}
 		}
 		
 	}
 	
-	private void SetBombs(int width, int height, int bombNum)
+	private void SetBombs(int width, int height, int bombNum, int offsetX, int offsetY)
 	{	
 		for(int i=0;i < bombNum;i++){
-			SetCell(new Vector2I(rng.RandiRange(0,width-1), rng.RandiRange(0,height-1)), TargetSourceId, Cells["bomb"]);
+			try{
+				Vector2I CellPos = new Vector2I(rng.RandiRange(0, width-1) + offsetX, rng.RandiRange(0,height-1) + offsetY);
+				Field.Add(CellPos, "bomb");
+			}
+			catch{
+				Vector2I CellPos = new Vector2I(rng.RandiRange(0, width-1) + offsetX, rng.RandiRange(0,height-1) + offsetY);
+				Field.Add(CellPos, "bomb");
+			}
 		}
-		
+		foreach (var bomb in Field){
+			SetCell(bomb.Key, TargetSourceId, Cells["bomb"]);
+		}
+	}
+	
+	
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton mouseClick && mouseClick.Pressed)
+		{	
+			Vector2 mousePositionFloat = ToLocal(GetGlobalMousePosition());
+			Vector2I mousePos = new Vector2I(Convert.ToInt32(mousePositionFloat.X), Convert.ToInt32(mousePositionFloat.Y));
+			
+			Vector2I TILE = ConvertToTile(mousePos);
+		}
+	}
+	public Vector2I ConvertToTile(Vector2I mousePosition){
+		int newTileX = (mousePosition.X - offsetX*16)/16;
+		int newTileY = (mousePosition.Y - offsetY*16)/16;
+		Vector2I Tile = new Vector2I(newTileX, newTileY);
+		GD.Print(newTileX + "  -  " + newTileY);
+		return Tile;
 	}
 }
